11527e20f1c2fccf2706a11af82ca5b0f0227979
Jordan Blackett <jordan.blackett@gmail.com>
2022-05-10 18:32:31 +0100

    S.A.M Messaging v0.4 - Sprite Generator

--- s.a.m-messaging-react/src/components/spritegenerator.js
+++ s.a.m-messaging-react/src/components/spritegenerator.js
@@ -1,7 +1,48 @@
+import { useState } from "react";
 import "./spritegenerator.css";
 
 function SpriteGenerator() {
-  return <div></div>;
+  // --- Draw Sprite Table ----
+
+  //function CreateTable() {
+  const rows = 8;
+  const cols = 8;
+  const spritegrid = [];
+
+  // Create Table
+  for (let i = 0; i < rows; i++) {
+    const tableColumn = [];
+    for (let j = 0; j < cols; j++) {
+      tableColumn.push(
+        <>
+          <SpriteSquare />
+        </>
+      );
+    }
+    spritegrid.push(<div style={{ display: "flex" }}>{tableColumn}</div>);
+  }
+  //}
+
+  return (
+    <div className="sprite-generator-container">
+      <div className="spritesquare-container">{spritegrid}</div>
+    </div>
+  );
+}
+
+function SpriteSquare() {
+  const [isActive, setActive] = useState("");
+
+  function test() {
+    setActive(!isActive);
+  }
+
+  return (
+    <div
+      className={isActive ? "spritesquare active" : "spritesquare"}
+      onClick={test}
+    ></div>
+  );
 }
 
 export default SpriteGenerator;
