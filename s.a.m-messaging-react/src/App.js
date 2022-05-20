017510f523d85f5e444485c32e1ae3b614c593a0
Jordan Blackett <jordan.blackett@gmail.com>
2022-05-20 18:27:53 +0100

    S.A.M Messaging v0.5 - Send Buttons

--- s.a.m-messaging-react/src/App.js
+++ s.a.m-messaging-react/src/App.js
@@ -2,6 +2,7 @@ import HeaderBanner from "./components/headerbanner";
 import StatusBanner from "./components/statusbanner";
 import SpriteGenerator from "./components/spritegenerator";
 import TextTerminal from "./components/textterminal";
+import SendButton from "./components/sendbutton";
 
 import MaxSimulator from "./components/max7219-simulator/maxsimulator";
 
@@ -21,6 +22,18 @@ function App() {
         {/* Text Terminal */}
         <div className="text-content-container">
           <TextTerminal />
+        </div>
+      </div>
+      {/* Send Button */}
+      <div className="send-button-container">
+        <div className="send-button-container-primary-background"></div>
+        <div className="send-button-container-secondary-background">
+          <div className="send-button-container-sprite">
+            <SendButton />
+          </div>
+          <div className="send-button-container-text">
+            <SendButton />
+          </div>
         </div>
       </div>
     </div>
---
+++ s.a.m-messaging-react/src/components/sendbutton.js
@@ -0,0 +1,11 @@
+import "./sendbutton.css";
+
+function SendButton() {
+  return (
+    <button type="button" className="send-button" id="send-message-button">
+      Send!
+    </button>
+  );
+}
+
+export default SendButton;
